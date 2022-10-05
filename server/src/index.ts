72850c617e4bb7e5c0270b4e3bf42f5f2fc51896
IkaWaAyuMu <pungpond4450@gmail.com>
2022-10-05 14:21:41 +0700

    Add ws demo

---
+++ server/src/index.ts
@@ -0,0 +1,28 @@
+import http from "http";
+import { connection, server } from "websocket";
+
+const httpServer : http.Server = http.createServer((req, res) => {
+    res.writeHead(404);
+    res.end();
+});
+httpServer.listen(8000, () => console.log("Server initiated and is listening on port 8000"));
+
+const wsServeer : server = new server({ httpServer, autoAcceptConnections: true });
+
+wsServeer.on('request', (req) => {
+    const connection: connection = req.accept('shooting-gallery', req.origin);
+    console.log((new Date()) + ': Connection accepted.');
+    connection.on('message', function(message) {
+        if (message.type === 'utf8') {
+            console.log('Received Message: ' + message.utf8Data);
+            connection.sendUTF(message.utf8Data);
+        }
+        else if (message.type === 'binary') {
+            console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
+            connection.sendBytes(message.binaryData);
+        }
+    });
+    connection.on('close', function(reasonCode, description) {
+        console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
+    });
+})
