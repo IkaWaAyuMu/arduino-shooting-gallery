012d4ff2ecb4da86ed8dc42992c60e04ee21afcc
IkaWaAyuMu <pungpond4450@gmail.com>
2022-10-05 16:00:53 +0700

    Add client definition

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,28 +1,59 @@
 import * as http from "http";
 import { connection, server } from "websocket";
+// Game setup
+let score: number = 0;
 
-const httpServer : http.Server = http.createServer((req, res) => {
+// server setup
+function sleep(ms: number) { return new Promise((resolve) => { setTimeout(resolve, ms); }); }
+
+const httpServer: http.Server = http.createServer((req, res) => {
     res.writeHead(404);
     res.end();
 });
 httpServer.listen(8000, () => console.log("Server initiated and is listening on port 8000"));
 
-const wsServer : server = new server({ httpServer, autoAcceptConnections: false });
+const wsServer: server = new server({ httpServer, autoAcceptConnections: false });
+
+const targetClients: connection[] = [];
+const gunClients: connection[] = [];
+const webClients: connection[] = [];
 
 wsServer.on('request', (req) => {
     const connection: connection = req.accept('', req.origin);
-    console.log((new Date()) + ': Connection accepted.');
-    connection.on('message', function(message) {
-        if (message.type === 'utf8') {
-            console.log('Received Message: ' + message.utf8Data);
-            connection.sendUTF(message.utf8Data);
+    console.log((new Date()) + `: Connection accepted from [ ${connection.remoteAddress} ].`);
+    connection.sendUTF("TYPE");
+    // Type specification
+    connection.on('message', (message) => {
+        if (message.type === "utf8" && message.utf8Data === "TARGET")  {
+            console.log(`Add ${connection.remoteAddress} to target`);
+            targetClients.push(connection);
         }
-        else if (message.type === 'binary') {
-            console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
-            connection.sendBytes(message.binaryData);
+        else if (message.type === "utf8" && message.utf8Data === "GUN") {
+            console.log(`Add ${connection.remoteAddress} to gun`);
+            gunClients.push(connection);
+        }
+        else if (message.type === "utf8" && message.utf8Data === "WEB") {
+            console.log(`Add ${connection.remoteAddress} to web`);
+            webClients.push(connection);
         }
     });
-    connection.on('close', function(reasonCode, description) {
-        console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
-    });
+
+    //Update from TARGET
+    connection.on('message', (message) => {
+        if (!targetClients.includes(connection)) return;
+
+        // Update score to web
+        if (message.type === "utf8" && message.utf8Data === "SCORE") {
+            score++
+            webClients.forEach(client => {client.send(JSON.stringify({score}));});
+        }
+    })
+
+    //Update from WEB
+    connection.on('message', (message) => {
+        if (!webClients.includes(connection)) return;
+        // Message sequence to target.
+        if (message.type === "utf8" && message.utf8Data === "START") targetClients.forEach(client => { client.send("START"); });
+        else if (message.type === "utf8" && message.utf8Data === "STOP") targetClients.forEach(client => { client.send("STOP"); });
+    })
 })
